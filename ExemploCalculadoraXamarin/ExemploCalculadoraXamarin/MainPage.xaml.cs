20a3006a639a7b17f33ac744b050f7bd261be3b8
LabInfo_2022 <LabInfo_2022@TERROA1MPROF>
2022-03-21 19:56:38 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploCalculadoraXamarin/ExemploCalculadoraXamarin/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExemploCalculadoraXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            int numero, outroNumero, resultado;
+
+            numero = int.Parse(txtNumero.Text);
+            outroNumero = int.Parse(txtOutroNumero.Text);
+
+            resultado = numero + outroNumero;
+
+            DisplayAlert("Calcular", $"{numero} + {outroNumero} = {resultado}", "OK");
+
+            txtNumero.Text = String.Empty;
+            txtOutroNumero.Text = "";
+        }
+    }
+}
